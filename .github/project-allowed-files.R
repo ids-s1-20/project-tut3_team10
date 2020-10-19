1819845ecad31ce9be5a3a0edd3dc9faae4eb690
Mine Cetinkaya-Rundel <cetinkaya.mine@gmail.com>
2020-10-20 00:26:21 +0100

    Initial commit

---
+++ .github/project-allowed-files.R
@@ -0,0 +1,23 @@
+allowed_files = c(".gitignore",
+                  "_config.yml",
+                  "data",
+                  "presentation",
+                  "project.Rproj",
+                  "proposal",
+                  "README.md",
+                  "README.Rmd",
+                  "README_files"
+                  )
+
+files = dir()
+disallowed_files = !files %in% allowed_files
+
+if (any(disallowed_files)) {
+  cat("Disallowed files found:\n")
+  cat("  (remove the following files from your repo)\n\n")
+
+  for(file in files[disallowed_files])
+    cat("*",file,"\n")
+
+  quit("no",1,FALSE)
+}
